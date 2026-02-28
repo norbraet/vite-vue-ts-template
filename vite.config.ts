0a959c3c95e0ff4903fb927f9921cf13f54f1bb9
Norbert Balaž <norbusmaximus@gmail.com>
2026-02-28 13:12:17 +0100

    chore(Setup): fixes vite port

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  server: {
+    port: 3000,
+  },
   plugins: [vue()],
   resolve: {
     alias: {
