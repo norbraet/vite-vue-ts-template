f036a1b49d9148a2e265edb5b038a00bc2b18efc
Norbert Balaž <norbusmaximus@gmail.com>
2026-05-01 17:44:03 +0200

    chore(Playwright): reduces browser variety for e2e tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -46,15 +46,6 @@ export default defineConfig({
     },
 
     {
-      name: 'Microsoft Edge',
-      use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    },
-    {
-      name: 'Google Chrome',
-      use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    },
-
-    {
       name: 'Mobile Chrome',
       use: { ...devices['Pixel 5'] },
     },
